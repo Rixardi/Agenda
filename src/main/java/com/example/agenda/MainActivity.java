e4c082f9b177b8af39bf99fcf31471223ea10727
Richard <93688490+Rixardi@users.noreply.github.com>
2022-09-27 22:43:01 -0300

    Add files via upload

---
+++ src/main/java/com/example/agenda/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_nome,et_telefone;
+    Button btn_gravar,btn_consultar,btn_fechar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_nome=(EditText) findViewById(R.id.et_nome);
+        et_telefone=(EditText) findViewById(R.id.et_telefone);
+        btn_gravar=(Button) findViewById(R.id.btn_gravar);
+        btn_consultar=(Button) findViewById(R.id.btn_consultar);
+        btn_fechar=(Button) findViewById(R.id.btn_fechar);
+    }
+}
